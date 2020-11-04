7311cb58af446a56af0dfcffc17930ba63c6fb3b
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-04 09:28:05 +0000

    Pseudo Code

--- for_loops.py
+++ for_loops.py
@@ -22,13 +22,13 @@
 # Task 1 - calculate year of birth
 name = input("What is your name?    ")
 age = int(input("How old are you?    "))
-birth_year = 2020 - age
+birth_year = 2020 - age # calculates the year of birth
 already_happened = input("Has your birthday happened this year?   (Y/N)    ")
 
-if already_happened.lower() == "y":
-    print("OMG", name, "you are", age, "years old so you were born in the year", birth_year)
-else:
-    birth_year -= 1
+if already_happened.lower() == "y": # if the user states their birthday happened this year
+    print("OMG", name, "you are", age, "years old so you were born in the year", birth_year) # print statement
+else:  # otherwise
+    birth_year -= 1 # -1 from birth year
     print("OMG", name, "you are", age, "years old so you were born in the year", birth_year)
 
 
@@ -36,22 +36,22 @@ else:
 
 #1)
 
-for i in range(10):
+for i in range(10): # for a value in the range 10 print "Hello
      print("Hello")
 
 
 #2 and 3
 
 list = []
-for i in range(10):
-    name = input("What name do you want to input?    ")
-    list.append(name)
+for i in range(10):   # for a value in the range 10
+    name = input("What name do you want to input?    ") # prompt user for a name
+    list.append(name) # adds name to the list
     continue
-print("Your list of names is", list)
+print("Your list of names is", list) # print the list
 
 list_names_lower = []
 for i in list:
-    list_names_lower.append(i.lower())
+    list_names_lower.append(i.lower()) # appends list_names_lower with lower case version of list
     continue
 print(list_names_lower)
 
@@ -59,11 +59,11 @@ print(list_names_lower)
 number_list = [2,5,6,7,8,9,10]
 odds = []
 evens = []
-for i in number_list:
-    if i % 2 != 0:
-        odds.append(i)
-    else:
-        evens.append(i)
+for i in number_list: # for every number in the list
+    if i % 2 != 0: # if there is a remainder when dividing by 2
+        odds.append(i) # add the number to the list odds
+    else: # otherwise
+        evens.append(i) # add the number to the list evens
 
 print("The odd numbers are", odds)
 print("The even numbers are", evens)
